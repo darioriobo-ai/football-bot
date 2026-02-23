1979e29585e9581032f74c3142d895800432cc68
darioriobo-ai <darioriobo@gmail.com>
2026-02-23 12:25:04 -0500

    Create leagues.js

---
+++ config/leagues.js
@@ -0,0 +1,13 @@
+module.exports = {
+  champions: { id: 2, name: 'Champions League' },
+  europa: { id: 3, name: 'Europa League' },
+  libertadores: { id: 13, name: 'Copa Libertadores' },
+  sudamericana: { id: 11, name: 'Copa Sudamericana' },
+  premier: { id: 39, name: 'Premier League' },
+  laliga: { id: 140, name: 'La Liga' },
+  seriea: { id: 135, name: 'Serie A' },
+  bundesliga: { id: 78, name: 'Bundesliga' },
+  ligue1: { id: 61, name: 'Ligue 1' },
+  liga_col: { id: 239, name: 'Liga Colombiana' },
+  copa_col: { id: 531, name: 'Copa Colombia' }
+};
